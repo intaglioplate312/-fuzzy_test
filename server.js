a7f96131cc837d75cc8f8aeeb027fdf670bbeb8c
intaglioplate312 <helenelizabeth@me.com>
2017-09-22 15:37:56 -0500

    Start of Mongoose Redux 2

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+var PORT = process.env.PORT || 8080;
+
+// Initialize Express
+var app = express();
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Database configuration with mongoose
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/mongoscraper" );
+mongoose.connect("mongodb://heroku_77wpkcvd:7ump1ovd4qobahqd3umts5kpv8@ds161860.mlab.com:61860/heroku_77wpkcvd");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+// get routes here
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Listen on port 8080
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+});
